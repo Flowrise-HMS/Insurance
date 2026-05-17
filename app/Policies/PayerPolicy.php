483eae4071c68eea5a34d83e1ba1052debd60ca0
musheabdulhakim <musheabdulhakim99@gmail.com>
2026-05-17 10:43:27 -0400

    Add PayerPolicy and gitignore

---
+++ app/Policies/PayerPolicy.php
@@ -0,0 +1,70 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Modules\Insurance\Policies;
+
+use Illuminate\Foundation\Auth\User as AuthUser;
+use Modules\Insurance\Models\Payer;
+use Illuminate\Auth\Access\HandlesAuthorization;
+
+class PayerPolicy
+{
+    use HandlesAuthorization;
+    
+    public function viewAny(AuthUser $authUser): bool
+    {
+        return $authUser->can('ViewAny Payer');
+    }
+
+    public function view(AuthUser $authUser, Payer $payer): bool
+    {
+        return $authUser->can('View Payer');
+    }
+
+    public function create(AuthUser $authUser): bool
+    {
+        return $authUser->can('Create Payer');
+    }
+
+    public function update(AuthUser $authUser, Payer $payer): bool
+    {
+        return $authUser->can('Update Payer');
+    }
+
+    public function delete(AuthUser $authUser, Payer $payer): bool
+    {
+        return $authUser->can('Delete Payer');
+    }
+
+    public function restore(AuthUser $authUser, Payer $payer): bool
+    {
+        return $authUser->can('Restore Payer');
+    }
+
+    public function forceDelete(AuthUser $authUser, Payer $payer): bool
+    {
+        return $authUser->can('ForceDelete Payer');
+    }
+
+    public function forceDeleteAny(AuthUser $authUser): bool
+    {
+        return $authUser->can('ForceDeleteAny Payer');
+    }
+
+    public function restoreAny(AuthUser $authUser): bool
+    {
+        return $authUser->can('RestoreAny Payer');
+    }
+
+    public function replicate(AuthUser $authUser, Payer $payer): bool
+    {
+        return $authUser->can('Replicate Payer');
+    }
+
+    public function reorder(AuthUser $authUser): bool
+    {
+        return $authUser->can('Reorder Payer');
+    }
+
+}

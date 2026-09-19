3506cb70e2d2a3d454e2826addbd68b77c9fa490
musheabdulhakim <musheabdulhakim99@gmail.com>
2026-09-19 19:36:50 +0000

    feat: place the insurance cluster in the Finance sidebar group

--- app/Filament/Clusters/Insurance/InsuranceCluster.php
+++ app/Filament/Clusters/Insurance/InsuranceCluster.php
@@ -5,10 +5,15 @@ namespace Modules\Insurance\Filament\Clusters\Insurance;
 use BackedEnum;
 use Filament\Clusters\Cluster;
 use Filament\Support\Icons\Heroicon;
+use Modules\Core\Enums\SidebarGroup;
 
 class InsuranceCluster extends Cluster
 {
     protected static ?string $slug = 'insurance-cluster';
 
-    protected static string|BackedEnum|null $navigationIcon = Heroicon::OutlinedSquares2x2;
+    protected static string|BackedEnum|null $navigationIcon = Heroicon::OutlinedShieldCheck;
+
+    protected static string|\UnitEnum|null $navigationGroup = SidebarGroup::Finance;
+
+    protected static ?int $navigationSort = 20;
 }

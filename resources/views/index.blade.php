bc36f1ee67a3045bd261086514c773012ca48386
musheabdulhakim <musheabdulhakim99@gmail.com>
2026-09-20 15:08:50 +0000

    docs: remove the stub insurance landing page and refresh README

--- resources/views/index.blade.php
+++
@@ -1,5 +0,0 @@
-<x-insurance::layouts.master>
-    <h1>Hello World</h1>
-
-    <p>Module: {!! config('insurance.name') !!}</p>
-</x-insurance::layouts.master>
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,11 @@
 <?php
 
-use Illuminate\Support\Facades\Route;
-
-Route::middleware(['auth', 'verified'])->group(function () {
-    Route::view('insurance', 'insurance::index')->name('insurance.index');
-});
+/*
+|--------------------------------------------------------------------------
+| Insurance web routes
+|--------------------------------------------------------------------------
+|
+| The Insurance module exposes no browser routes of its own; its UI lives in
+| the Filament "Insurance" cluster and its integrations in routes/api.php.
+|
+*/
